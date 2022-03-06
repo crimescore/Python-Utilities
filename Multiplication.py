efb6a59a2651b091fd216224654c929317eb55ab
PurpleProfessional <97543454+PurpleProfessional@users.noreply.github.com>
2022-03-05 23:42:55 -0500

    Update Multiplication.py

--- Multiplication.py
+++ Multiplication.py
@@ -1,4 +1,4 @@
-num1=int(input("What is the first number to add?"))
-num2=int(input("What is the second number to add?"))
+num1=int(input("What is the first number to multiply?"))
+num2=int(input("What is the second number to multiply?"))
 print(num1 * num2)
 input ("Press enter to exit")
